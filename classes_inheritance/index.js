268d1784d3370510f75b2e6180d0555aad03f511
YuliyaShu <juliette7373@gmail.com>
2022-06-14 15:43:39 +0300

    refactor: add curved brackets, semicolons, ets, change logic expressions

--- classes_inheritance/index.js
+++ classes_inheritance/index.js
@@ -1,20 +1,16 @@
-
-// task in README.md 
-
-
 class Builder {
   constructor(value) {
     this.value = value;
     console.log(this.value);
   }
 
-  plus(...n) {
-    this.value = Object.values(arguments).reduce((res, n) => res + n, (this.value));
+  plus() {
+    this.value = Object.values(arguments).reduce((res, unit) => res + unit, this.value);
     console.log(this.value);
     return this;
   }
 
-  minus(...n) {
+  minus() {
     console.log(this.value);
     return this;
   }
@@ -40,55 +36,59 @@ class IntBuilder extends Builder {
 
   plus(...n) {
     for (let param of arguments) {
-      if  (Number.isInteger(param) === false) 
+      if  (!Number.isInteger(param)) { 
         throw new Error('IntBuilder.plus() method: parameter should be integer');
+      }
     }
     return super.plus(...n);
   }
   
   mod(n) {
-    if (Number.isInteger(n) === false
-     || n <= 0) 
-     throw new Error('IntBuilder.mod() method: parameter should be integer and > 0');
+    if (!Number.isInteger(n) || n <= 0) {
+      throw new Error('IntBuilder.mod() method: parameter should be integer and > 0');
+    }
    this.value = this.value % n;
    console.log(this.value);
    return this;
   }
 
-  minus(...n) {
+  minus() {
     for (let param of arguments) {
-      if  (Number.isInteger(param) === false) 
-      throw new Error('IntBuilder.minus() method: parameter should be integer');
+      if  (!Number.isInteger(param)) {
+        throw new Error('IntBuilder.minus() method: parameter should be integer');
+      }
     }
-    this.value = Object.values(arguments).reduce((res, e) => res - e, this.value);
-    return super.minus(...n);
+    this.value = Object.values(arguments).reduce((res, numMinus) => res - numMinus, this.value);
+    console.log(this.value);
+    return this;
   }
 
   multiply(n) {
-    if (Number.isInteger(n) === false
-     || n <= 0) 
-     throw new Error('IntBuilder.multiply() method: parameter should be integer and > 0');
+    if (!Number.isInteger(n) || n <= 0) {
+      throw new Error('IntBuilder.multiply() method: parameter should be integer and > 0');
+    }
     this.value = n * this.value;
-    return super.multiply(n);
+    console.log(this.value);
+    return this;
   }
 
   divide(n) {
-    if (Number.isInteger(n) === false
-     || n <= 0) 
-     throw new Error('IntBuilder.divide() method: parameter should be integer and > 0');
+    if (!Number.isInteger(n) || n <= 0) {
+      throw new Error('IntBuilder.divide() method: parameter should be integer and > 0');
+    }
     this.value = Math.floor(this.value / n);
-    return super.divide(n);;
+    console.log(this.value);
+    return this;
   }
 
   static random(from, to) {
-    if (Number.isInteger(from) === false
-     || from < 0
-     || Number.isInteger(to) === false
-     || to < 0) 
-     throw new Error('IntBuilder.random() method: parameter(s) should be integers and >= 0');
-    if (to <= from)
-      throw new Error('IntBuilder.random() method: second parameter should be > than first parameter')
-
+    if (!Number.isInteger(from) || from < 0
+      ||!Number.isInteger(to) || to < 0) {
+        throw new Error('IntBuilder.random() method: parameter(s) should be integers and >= 0');
+      }
+    if (to <= from) {
+      throw new Error('IntBuilder.random() method: second parameter should be > than first parameter');
+    }
     return Math.round(Math.random() * (to - from) + from);
   }
 }
@@ -104,52 +104,53 @@ StringBuilder.prototype.constructor = StringBuilder;
 
 
 StringBuilder.prototype.remove = function(str) {
-  if (typeof str !== 'string') 
+  if (typeof str !== 'string') {
     throw new Error('StringBuilder.remove() method: parameter should be type of string');
+  }
   this.value = this.value.split(str).join('');
   console.log(this.value);
   return this;
 }
 
 StringBuilder.prototype.sub = function(from, n) {
-  if (Number.isInteger(from) === false
-   || from < 0
-   || Number.isInteger(n) === false
-   || n < 0) 
-   throw new Error('StringBuilder.sub() method: parameter(s) should be integers and >= 0');
-  if (from >= this.value.length) 
-    throw new Error(`StringBuilder.sub() method: first parameter should be < than ${this.value.length} - stored value length`)
+  if (!Number.isInteger(from) || from < 0
+   || !Number.isInteger(n) || n < 0) {
+    throw new Error('StringBuilder.sub() method: parameter(s) should be integers and >= 0');
+   }
+  if (from >= this.value.length) {
+    throw new Error(`StringBuilder.sub() method: first parameter should be < than ${this.value.length} - stored value length`);
+  }
   this.value = this.value.substring(from, from + n);
   console.log(this.value);
   return this;
 }
 
 StringBuilder.prototype.minus = function(n){
-  if (Number.isInteger(n) === false
-   || n < 0) 
-   throw new Error('StringBuilder.minus() method:parameter should be integer and >= 0');
+  if (!Number.isInteger(n) || n < 0) {
+    throw new Error('StringBuilder.minus() method:parameter should be integer and >= 0');
+  }
   this.value = this.value.substring(0, this.value.length - n);
   console.log(this.value);
   return this;
 }
 
 StringBuilder.prototype.multiply = function(n) {
-  if (Number.isInteger(n) === false
-   || n <= 0) 
-   throw new Error('StringBuilder.multiply() method: parameter should be integer and > 0');
+  if (!Number.isInteger(n) || n <= 0) {
+    throw new Error('StringBuilder.multiply() method: parameter should be integer and > 0');
+  }
   let str = '';
   for (let i =0; i < n; i++) {
-       str += this.value;
-    }
+    str += this.value;
+  }
   this.value = str;
   console.log(this.value)
   return this;
 }
 
 StringBuilder.prototype.divide = function(n) {
-  if (Number.isInteger(n) === false
-   || n <= 0) 
-   throw new Error('StringBuilder.divide() method:parameter should be integer and > 0');
+  if (!Number.isInteger(n) || n <= 0) {
+    throw new Error('StringBuilder.divide() method:parameter should be integer and > 0');
+  }
   this.value = this.value.substring(0, Math.floor(this.value.length / n));
   console.log(this.value)
   return this;
@@ -170,6 +171,9 @@ intBuilder
   .mod(3)                     // 1;
   .get();                    // -> 1;
 
+
+
+
 console.log('\nTests ES5')
 let strBuilder = new StringBuilder('Hello'); // 'Hello';
 strBuilder
@@ -181,4 +185,3 @@ strBuilder
   .sub(1,1)                                  // 'e';
   .get();                                    // -> 'e';
   
-
