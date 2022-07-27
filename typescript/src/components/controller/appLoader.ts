a728dc9f16100f746531f5a491f609c7edc41b82
YuliyaShu <juliette7373@gmail.com>
2022-07-27 10:36:32 +0200

    feat: migrate appLoader.js to ts

--- typescript/src/components/controller/appLoader.ts
+++ typescript/src/components/controller/appLoader.ts
@@ -2,8 +2,8 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+        super('https://nodenews.herokuapp.com/', {
+            apiKey: 'e4ade38a77d04d8081ae2a42230f6fa2',
         });
     }
 }
