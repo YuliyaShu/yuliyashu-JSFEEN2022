ba8076fa1533a9a38d3095353ba4c885cc129fef
YuliyaShu <juliette7373@gmail.com>
2022-07-27 11:00:54 +0200

    feat: migrate appView.js to ts

---
+++ typescript/src/components/view/appView.ts
@@ -0,0 +1,25 @@
+import { NewsInt, NewsUnit } from '../interfaces/newsInterface';
+import { SourceInt, SourcesUnit } from '../interfaces/sourcesInterface';
+import News from './news/news';
+import Sources from './sources/sources';
+
+export class AppView {
+    sources: Sources;
+    news: News;
+    constructor() {
+        this.news = new News();
+        this.sources = new Sources();
+    }
+
+    drawNews(data?: NewsInt): void {
+        const values: NewsUnit[] = data?.articles ? data.articles : [];
+        this.news.draw(values as NewsUnit[]);
+    }
+
+    drawSources(data?: SourceInt): void {
+        const values: SourcesUnit[] = data?.sources ? data.sources : [];
+        this.sources.draw(values as SourcesUnit[]);
+    }
+}
+
+export default AppView;
