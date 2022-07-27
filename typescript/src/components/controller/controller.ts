87fcd81f246d36d66f6eaf1b00b625bfce7c8fb7
YuliyaShu <juliette7373@gmail.com>
2022-07-27 10:43:24 +0200

    feat: migrate controller.js to ts

--- typescript/src/components/controller/controller.ts
+++ typescript/src/components/controller/controller.ts
@@ -1,37 +1,40 @@
+import { NewsInt } from '../interfaces/newsInterface';
+import { SourceInt } from '../interfaces/sourcesInterface';
+import { CallBack } from '../types/callbackType';
 import AppLoader from './appLoader';
 
 class AppController extends AppLoader {
-    getSources(callback) {
-        super.getResp(
-            {
-                endpoint: 'sources',
-            },
-            callback
-        );
+    getSources(callback: CallBack<SourceInt>): void {
+        super.getResp<SourceInt>({ endpoint: 'sources' }, callback);
     }
 
-    getNews(e, callback) {
+    getNews(e: Event, callback: CallBack<NewsInt>): void {
         let target = e.target;
         const newsContainer = e.currentTarget;
+        if (!(target instanceof HTMLElement) || !(newsContainer instanceof HTMLElement)) {
+            return;
+        }
+        const sourceId = target.getAttribute('data-source-id');
 
         while (target !== newsContainer) {
-            if (target.classList.contains('source__item')) {
-                const sourceId = target.getAttribute('data-source-id');
-                if (newsContainer.getAttribute('data-source') !== sourceId) {
-                    newsContainer.setAttribute('data-source', sourceId);
-                    super.getResp(
-                        {
-                            endpoint: 'everything',
-                            options: {
-                                sources: sourceId,
+            if (target instanceof HTMLElement) {
+                if (target.classList.contains('source__item')) {
+                    if (newsContainer && sourceId && newsContainer.getAttribute('data-source') !== sourceId) {
+                        newsContainer.setAttribute('data-source', sourceId);
+                        super.getResp<NewsInt>(
+                            {
+                                endpoint: 'everything',
+                                options: {
+                                    sources: sourceId,
+                                },
                             },
-                        },
-                        callback
-                    );
+                            callback
+                        );
+                    }
+                    return;
                 }
-                return;
+                target = <HTMLElement>target.parentNode;
             }
-            target = target.parentNode;
         }
     }
 }
