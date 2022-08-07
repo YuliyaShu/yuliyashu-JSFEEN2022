6e448f6321648d91de739db1bdd08b2f315cdbc5
YuliyaShu <juliette7373@gmail.com>
2022-08-07 15:41:12 +0200

    feat: compact method

--- unit-tests/src/compact.js
+++ unit-tests/src/compact.js
@@ -1,5 +1,13 @@
 const compact = (arr) => {
-   
+    if (!Array.isArray(arr)) throw new Error('First argument should be an ARRAY!');
+    if (!arr.length) throw new Error('ARRAY is empty!');
+
+    let res = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i]) res = [...res, arr[i]];
+    }
+    return res;
   };
 
   module.exports = compact;
