bfdfd3bb835967b160cd3515353796a1f0760004
YuliyaShu <juliette7373@gmail.com>
2022-08-07 18:31:43 +0200

    feat: dropWhile method

---
+++ unit-tests/src/dropWhile.js
@@ -0,0 +1,38 @@
+const dropWhile = (array, predicate) => {
+  if (!Array.isArray(array)) throw new Error('First argument should be an ARRAY!');
+  if (!array.length) throw new Error('ARRAY is empty!');
+  if (predicate === null) throw new Error('Second argument should not be NULL!');
+  if (!(typeof predicate === 'function') &&
+      !(typeof predicate === 'object')   &&
+      !(typeof predicate === 'string')) throw new Error('Second argument should be a FUNCTION, OBJECT, ARRAY or STRING!');
+
+  let res = [];
+  let flag = true;
+
+
+  for (let item of array) {
+    if (flag) {
+      if (predicate instanceof Function)
+        flag = predicate(item)
+      else if (predicate instanceof Array) 
+        flag = (item[predicate[0]] === predicate[1])
+      else if (typeof predicate === 'string')
+        flag = (item[predicate])
+      else if (predicate instanceof Object) {
+        for (let key of Object.keys(item)) {
+          flag = (item[key] === predicate[key]);
+          if (!flag) break;
+        }
+      } 
+      if (!(flag)) res = [item];
+      continue;
+    }
+    res = [...res, item];
+  }
+
+  return res;
+
+}
+
+module.exports = dropWhile;
+  
