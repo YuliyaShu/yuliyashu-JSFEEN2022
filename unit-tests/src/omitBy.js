a921b72c624eab42411330fe524129c1f5e63dc4
YuliyaShu <juliette7373@gmail.com>
2022-08-08 22:31:24 +0200

    feat: omitBy method

---
+++ unit-tests/src/omitBy.js
@@ -0,0 +1,17 @@
+const omitBy = (object, predicate) => {
+    if (!(object instanceof Object)) throw new Error('First argument should be an OBJECT!');
+    if (!Object.keys(object).length) throw new Error('OBJECT is empty!');
+    if (!(predicate === undefined) && !(typeof predicate === 'function')) throw new Error('Second argument should be a FUNCTION!');
+  
+    let res = Object.assign(object);
+    if (predicate === undefined) return res;
+  
+    for (let key in object) {
+      if (predicate(object[key])) delete res[key];
+    }
+  
+    return res;
+  }
+  
+  module.exports = omitBy;
+    
