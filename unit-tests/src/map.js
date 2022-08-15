74fffd824a5d6d5e4b1c1f66bda93bb3075a4c75
YuliyaShu <juliette7373@gmail.com>
2022-08-15 19:00:55 +0200

    feat: map method

---
+++ unit-tests/src/map.js
@@ -0,0 +1,40 @@
+const map = (collection, iteratee) => {
+  if (!(typeof collection === 'object')) throw new Error('First argument should be a COLLECTION!');
+  if ((collection instanceof Array && !collection.length) || (collection instanceof Object && !Object.keys(collection).length))
+    throw new Error('COLLECTION is empty!');
+  if (iteratee === null) throw new Error('Second argument should not be NULL!');
+  if (!(typeof iteratee === 'function') && !(typeof iteratee === 'string'))
+    throw new Error('Second argument should be a FUNCTION or STRING!');
+    
+  let res = [];
+
+  if (collection instanceof Array) {
+    for (let item of collection) {
+      if ((item instanceof Object)) {
+        for (let key in item) {
+          if (key === iteratee) {
+            res = [...res, item[key]];
+          }
+        }
+      } else
+        res = [...res, iteratee(item)];
+    }
+  } else {
+    if (iteratee instanceof Function) {
+      for (let item of Object.values(collection)) {
+        res = [...res, iteratee(item)];
+      }
+    } else {
+      for (let key in collection) {
+        if (key === iteratee) {
+          res = [...res, collection[key]];
+        }
+      }
+    }
+  }
+
+  return res;
+}
+
+module.exports = map;
+  
