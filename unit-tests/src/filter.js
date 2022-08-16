0712e5a1a85930fe7535988f271900d6317a1d48
YuliyaShu <juliette7373@gmail.com>
2022-08-16 13:19:04 +0200

    refactor: add curle brackets part 2

--- unit-tests/src/filter.js
+++ unit-tests/src/filter.js
@@ -1,10 +1,18 @@
 const filter = (array, predicate) => {
-  if (!Array.isArray(array)) throw new Error('First argument should be an ARRAY!');
-  if (!array.length) throw new Error('ARRAY is empty!');
-  if (predicate === null) throw new Error('Second argument should not be NULL!');
+  if (!Array.isArray(array)) {
+    throw new Error('First argument should be an ARRAY!');
+  } 
+  if (!array.length) {
+    throw new Error('ARRAY is empty!');
+  } 
+  if (predicate === null) {
+    throw new Error('Second argument should not be NULL!');
+  } 
   if (!(typeof predicate === 'function') &&
       !(typeof predicate === 'object')   &&
-      !(typeof predicate === 'string')) throw new Error('Second argument should be a FUNCTION, OBJECT, ARRAY or STRING!');
+      !(typeof predicate === 'string')) {
+        throw new Error('Second argument should be a FUNCTION, OBJECT, ARRAY or STRING!');
+      } 
 
   let res = [];
   let flag = true;
@@ -12,20 +20,28 @@ const filter = (array, predicate) => {
 
   for (let item of array) {
     flag = false;
-    if (predicate instanceof Function)
-      flag = predicate(item)
-    else if (predicate instanceof Array) 
-      flag = (item[predicate[0]] === predicate[1])
-    else if (typeof predicate === 'string')
-      flag = (item[predicate])
+    if (predicate instanceof Function) {
+      flag = predicate(item);
+    }
+    else if (predicate instanceof Array) {
+      flag = (item[predicate[0]] === predicate[1]);
+    } 
+    else if (typeof predicate === 'string') {
+      flag = (item[predicate]);
+    }
+      
     else if (predicate instanceof Object) {
       for (let key of Object.keys(predicate)) {
         flag = (item[key] === predicate[key]);
-        if (!flag) break;
+        if (!flag) {
+          break;
+        } 
       }
     }
 
-    if (flag) res = [...res, item];
+    if (flag) {
+      res = [...res, item];
+    } 
   }
 
   return res;
