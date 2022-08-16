ab658a426f7c3c6ac52e8e8520ceb547236f1608
YuliyaShu <juliette7373@gmail.com>
2022-08-16 13:29:53 +0200

    refactor: add curle brackets part 5

--- unit-tests/src/pick.js
+++ unit-tests/src/pick.js
@@ -1,9 +1,17 @@
 const pick = (object, paths) => {
-    if (!(object instanceof Object)) throw new Error('First argument should be an OBJECT!');
-    if (!Object.keys(object).length) throw new Error('OBJECT is empty!');
+    if (!(object instanceof Object)) {
+      throw new Error('First argument should be an OBJECT!');
+    } 
+
+    if (!Object.keys(object).length) {
+      throw new Error('OBJECT is empty!');
+    } 
+
     if (!(paths === undefined)       &&
         !(typeof paths === 'string') && 
-        !(paths instanceof Array)) throw new Error('Second argument should be a STRING or an ARRAY of STRINGS!');
+        !(paths instanceof Array)) {
+          throw new Error('Second argument should be a STRING or an ARRAY of STRINGS!');
+    } 
   
     if (paths === undefined) return {};
     let res = Object.assign(object);
@@ -11,13 +19,19 @@ const pick = (object, paths) => {
   
     for (let key in object) {
       if (typeof paths === 'string') {
-        if (key !== paths) delete res[key];
+        if (key !== paths) {
+          delete res[key];
+        } 
       } else {
         flag = false;
         for (path of paths) {
-          if (key === path) flag = true;
+          if (key === path) {
+            flag = true;
+          } 
         }
-        if (!flag) delete res[key];
+        if (!flag) {
+          delete res[key];
+        } 
       }
     }
   
