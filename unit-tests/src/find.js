4936bff5a1f8090215f5c843c9f38f880f01ff63
YuliyaShu <juliette7373@gmail.com>
2022-08-16 13:21:49 +0200

    refactor: add curle brackets part 3

--- unit-tests/src/find.js
+++ unit-tests/src/find.js
@@ -1,12 +1,29 @@
 const find = (collection, predicate, fromIndex = 0) => {
-  if (!(typeof collection === 'object')) throw new Error('First argument should be a COLLECTION!');
-  if (collection === null || !collection.length) throw new Error('COLLECTION is empty!');
-  if (predicate === null) throw new Error('Second argument should not be NULL!');
+  if (!(typeof collection === 'object')) {
+    throw new Error('First argument should be a COLLECTION!');
+  } 
+
+  if (collection === null || !collection.length) {
+    throw new Error('COLLECTION is empty!');
+  }
+
+  if (predicate === null) {
+    throw new Error('Second argument should not be NULL!');
+  } 
+
   if (!(typeof predicate === 'function') &&
       !(typeof predicate === 'object')   &&
-      !(typeof predicate === 'string')) throw new Error('Second argument should be a FUNCTION, OBJECT, ARRAY or STRING!');
-  if (!Number.isInteger(fromIndex)) throw new Error('Third argument should be an INTEGER!');
-  if (fromIndex < 0) throw new Error('FromIndex should not be a NEGATIVE!');
+      !(typeof predicate === 'string')) {
+        throw new Error('Second argument should be a FUNCTION, OBJECT, ARRAY or STRING!');
+  } 
+
+  if (!Number.isInteger(fromIndex)) {
+    throw new Error('Third argument should be an INTEGER!');
+  } 
+
+  if (fromIndex < 0) {
+    throw new Error('FromIndex should not be a NEGATIVE!');
+  } 
     
   let res;
   let flag = true;
@@ -16,20 +33,27 @@ const find = (collection, predicate, fromIndex = 0) => {
   for (let i = fromIndex; i < collection.length; i++) {
     flag = false;
     item = collection[i];
-    if (predicate instanceof Function)
-      flag = predicate(item)
-    else if (predicate instanceof Array) 
-      flag = (item[predicate[0]] === predicate[1])
-    else if (typeof predicate === 'string')
-      flag = (item[predicate])
+    if (predicate instanceof Function) {
+      flag = predicate(item);
+    }
+    else if (predicate instanceof Array) {
+      flag = (item[predicate[0]] === predicate[1]);
+    }
+    else if (typeof predicate === 'string') {
+      flag = (item[predicate]);
+    }
     else if (predicate instanceof Object) {
       for (let key of Object.keys(predicate)) {
         flag = (item[key] === predicate[key]);
-        if (!flag) break;
+        if (!flag) {
+          break;
+        } 
       }
     }
 
-    if (flag) return item;
+    if (flag) {
+      return item;
+    } 
   }
 
   return res;
